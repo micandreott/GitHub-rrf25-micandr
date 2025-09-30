deb80d43e07eae4b6dc3d72b9d6abf68e0d9fd39
micandreott <60824262+micandreott@users.noreply.github.com>
2025-09-30 16:44:48 -0400

    updating data path

--- R/Code/main.R
+++ R/Code/main.R
@@ -21,8 +21,8 @@ library(RColorBrewer) # color palettes
 # this is the second root of the project, the first root is the code whose directory 
 # is already being handled by the rstudio project.
 
-data_path <- "the path for your data"
+data_path <- "C:/Users/wb567588/OneDrive - WBG/Documents/RRF-2025/DataWork/Data"
 
 # Run the R scripts ----
 
-source("the path of your scripts")
+source("Code/01-processing-data.R")
